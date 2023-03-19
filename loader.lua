ad4dda34d22e60be0882e5d7a77ba2d3c3743757
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-03-18 20:13:11 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,13 @@
 local VoidwareOwner = loadstring(game:HttpGet("https://raw.githubusercontent.com/SystemXVoid/whitelist/main/owner", true))()
 local VoidwareINF = loadstring(game:HttpGet("https://raw.githubusercontent.com/SystemXVoid/whitelist/main/inf", true))()
 local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
-for i,v in pairs(HWID) do
-    if v == VoidwareOwner then
+for i,v in pairs(VoidwareOwner) do
+    if v == HWID then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/SystemXVoid/whitelist/main/ChatTags/owner", true))()
     end
 end
-	for i,v in pairs(HWID) do
-		if v == VoidwareINF then
+	for i,v in pairs(VoidwareINF) do
+		if v == HWID then
 			loadstring(game:HttpGet("https://raw.githubusercontent.com/SystemXVoid/whitelist/main/ChatTags/inf", true))()
 		end
 		end
