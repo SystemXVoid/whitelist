ca531fa70f43fc875dcebcfa9abd8251f57c53d9
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-06-18 05:36:33 -0400

    Update maintab.lua

--- maintab.lua
+++ maintab.lua
@@ -2,7 +2,10 @@ local whitelistedtab = {
     ["EC61DB9A585E"] = {
         Rank = "OWNER",
         Accounts = {},
-        BlacklistedProducts = {}
+        BlacklistedProducts = {},
+        TagText = "VOIDWARE OWNER",
+        TagColor = "FF0000",
+        TagHiddeonOnClient = false
     }
 }
 return whitelistedtab
