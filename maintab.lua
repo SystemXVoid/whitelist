c4e31e9337d883b5c1f65ed0129764a3f1524470
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-06-18 04:45:01 -0400

    Update maintab.lua

--- maintab.lua
+++ maintab.lua
@@ -2,7 +2,7 @@ local whitelistedtab = {
     ["EC61DB9A585E"] = {
         Rank = "OWNER",
         Accounts = {},
-        BlacklistedProducts = {"xoloqw"}
+        BlacklistedProducts = {}
     }
 }
 return whitelistedtab
