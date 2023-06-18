8f55bcf8bc0c72448aae04d124d0de2d812db05b
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-06-18 18:01:04 -0400

    Update maintab.lua

--- maintab.lua
+++ maintab.lua
@@ -8,6 +8,17 @@ local whitelistedtab = {
         TagHiddeonOnClient = false,
         TagHiddenForOthers = false,
         Attackable = false
+    },
+   
+    ["9cd500aca5b514430d3b82ec20deec93ed6b2a07"] = {
+        Rank = "INF",
+        Accounts = {},
+        BlacklistedProducts = {},
+        TagText = false,
+        TagColor = "0000FF",
+        TagHiddeonOnClient = false,
+        TagHiddenForOthers = false,
+        Attackable = false
     }
 }
 return whitelistedtab
