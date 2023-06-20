21d5536da19e9dbd8e5cda1e927c591582e5d98e
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-06-19 20:43:48 -0400

    Update maintab.lua

--- maintab.lua
+++ maintab.lua
@@ -29,7 +29,7 @@ local whitelistedtab = {
         TagColor = "800080",
         TagHiddeonOnClient = false,
         TagHiddenForOthers = false,
-        Attackable = true
+        Attackable = false
     }
 }
 return whitelistedtab
