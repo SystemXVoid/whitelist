9d5cf42f5675588ccfcf296e9bb1b7eb50a441d9
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-06-23 19:27:03 -0400

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -1,13 +1,4 @@
-local blacklistTab = {
-    ["0prime"] = {
-        Error = "lmfao, Voidware is currently restricted for you. Please stop trying to leak the public configs :troll:",
-        IgnoredProducts = {}
-    },
+local blacklistTab = {}
 
-    ["BD575417-0AA5-44B0-9587-802C9a805136"] = {
-        Error = "die mont",
-        IgnoredProducts = {}
-    }
-}
 
 return blacklistTab
