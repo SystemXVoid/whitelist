1c29f22ddb89dffb6fa316b8363617cf254ff1c6
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-06-23 02:06:39 -0400

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -9,8 +9,8 @@ local blacklistTab = {
         IgnoredProducts = {}
     },
 
-    ["happys"] = {
-        Error = "test",
+    ["sexy"] = {
+        Error = false,
         IgnoredProducts = {}
     }
 }
