a2485c222a467a34886099d6a63694e1e0f7d9c6
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-06-23 01:36:55 -0400

    yes

--- blacklist.lua
+++ blacklist.lua
@@ -7,6 +7,10 @@ local blacklistTab = {
     ["BD575417-0AA5-44B0-9587-802C9a805136"] = {
         Error = "die mont",
         IgnoredProducts = {}
+    },
+    ["Seroid"] = {
+        Error = "Test",
+        IgnoredProducts = {}
     }
 }
 
