0b0dbebd7c11c72207c86eff5742a8796f511afc
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-06-23 02:28:17 -0400

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -1,5 +1,5 @@
 local blacklistTab = {
-    ["Ser"] = {
+    ["happy"] = {
         Error = "lmfao, Voidware is currently restricted for you. Please stop trying to leak the public configs :skull:",
         IgnoredProducts = {}
     },
