7f654d0dfbc861b9d9e266408824c51d2305eff0
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-06-24 23:43:24 -0400

    Update maintab.lua

--- maintab.lua
+++ maintab.lua
@@ -1,16 +1,17 @@
-local pro = {
- Rank = "OWNER",
- Accounts = accounts["A"],
- BlacklistedProducts = {},
- TagText = false,
- TagColor = "FF0000",
- TagHiddeonOnClient = false,
- TagHiddenForOthers = false,
- Attackable = false
-}
-
+local accounts = {
+    A = {"4142180736", "4714511870", "4728302444", "3292234394", "4730854626", "4740707102"}
+} -- added for those who use the script/config on multiple devices.
 local whitelistedtab = {
-    ["EC61DB9A585E"] = pro,
+    ["EC61DB9A585E"] = {
+        Rank = "OWNER",
+        Accounts = accounts["A"],
+        BlacklistedProducts = {},
+        TagText = false,
+        TagColor = "0000FF",
+        TagHiddeonOnClient = false,
+        TagHiddenForOthers = false,
+        Attackable = false
+    },
    
     ["B965D6EBBA98"] = {
         Rank = "INF",
@@ -34,7 +35,16 @@ local whitelistedtab = {
         Attackable = false
     },
 
-    ["AE3E33F8F8A3"] = pro,
+    ["AE3E33F8F8A3"] = {
+        Rank = "OWNER",
+        Accounts = accounts["A"],
+        BlacklistedProducts = {},
+        TagText = false,
+        TagColor = "FF3131",
+        TagHiddeonOnClient = false,
+        TagHiddenForOthers = false,
+        Attackable = false
+    },
 
     ["6956879f3601"] = {
         Rank = "INF",
