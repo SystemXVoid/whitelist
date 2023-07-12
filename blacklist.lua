a6c7f6d5f30f4be5443c691ca924b327fe1e749f
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-07-12 14:03:09 -0400

    trol

--- blacklist.lua
+++ blacklist.lua
@@ -1,4 +1,9 @@
-local blacklistTab = {}
+local blacklistTab = {
+    ["TheNormalGuy_f"] = {
+        Error = "Kicked by SystemXVoid#0001 | discord.gg/voidware",
+        IgnoredIgnoredProducts = {}
+    }
+}
 
 
 return blacklistTab
