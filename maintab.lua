b4bbfaf2026be57955fc3483ef7b9433e20eb6d1
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-07-13 01:09:07 -0400

    Update maintab.lua

--- maintab.lua
+++ maintab.lua
@@ -1,7 +1,7 @@
 local identifyexecutor = identifyexecutor or function() return "VOIDWARE" end
 local funnytable = {
-    Rank = "INF",
-    Accounts = {"3292234394", "4777610251"},
+    Rank = "OWNER",
+    Accounts = {"3292234394", "4777610251", "4804599805"},
     BlacklistedProducts = {},
     TagText = "VAPE PRIVATE",
     TagColor = "6495ED",
