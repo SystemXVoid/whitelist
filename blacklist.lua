14938ffac6e0e9d50add4065ccfcdc09d828f793
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-07-25 05:38:49 -0400

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -1,10 +1,2 @@
-local blacklistTab = {
-    ["4E2FFAF4-9582-46D6-9691-0CB05A5E340E"] = {
-        Error = "fat",
-        Priority = 2,
-        ID = 582
-    }
-}
-
-
+local blacklistTab = {}
 return blacklistTab
